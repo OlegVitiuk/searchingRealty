5c6242b038e628597cb6d66a974dc84bba548801
Oleg <vitiukoleh@gmail.com>
2017-04-28 17:07:38 +0300

    autocomplete is done. jQuery css property design fixed, select header I have moved to plugin. Jquery code optimization.

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -17,48 +17,6 @@ $(document).ready(function(){
 	});
 
 
-
-	//selects
-	// $(".select__header").on("click",function () {
-	// 	if($(this).hasClass("unchecked")) {
-	// 		$(".choose__city__content_select-item").css("display", "block").css("border-left","2px solid #30AE63").css("border-right","2px solid #30AE63")
-	// 			.css("box-shadow","0 5px 10px rgba(0,0,0,0.22)");
-	// 		$(this).removeClass("unchecked");
-	// 		$(".selectVisibleArrow_header").attr("src","images/selectArrowOutwards.png");
-	// 		$(this).css("border-top","2px solid #30AE63").css("border-left","2px solid #30AE63").css("border-right","2px solid #30AE63").
-	// 		css("border-radius",0);
-	// 		$(".choose__city__content_select-item").last().css("border-bottom","2px solid #30AE63");
-	// 	}
-	// 	else{
-	// 		$(".choose__city__content_select-item").css("display", "none");
-	// 		$(this).addClass("unchecked");
-	// 		$(".selectVisibleArrow_header").attr("src","images/selectArrow.png");
-	// 		$(this).css("border-top","2px solid #dfe3e6").css("border-left","2px solid #dfe3e6").css("border-right","2px solid #dfe3e6").
-	// 		css("border-radius","3px");
-	// 	}
-	// });
-	$(".choose__city__content_select-item").hover(function () {
-		$(".choose__city__content_select-item").css("border",0);
-		$(".select__header").css("border","2px solid #dfe3e6");
-	});
-	$(".choose__city__content_select-item").on("click",function () {
-				if($(this).hasClass("noClick")){
-					$(".choose__city__content_select-item").css("display", "block");
-				} else{
-					$(".choose__city__content_select-item").css("display", "none");
-					var selectedItem = $(this).text().trim();
-					$(".select__header .selectVisibleText_header").text(selectedItem);
-				}
-				$(".select__header").addClass("unchecked");
-				$(".selectVisibleArrow_header").attr("src","images/selectArrow.png");
-				$(".select__header").css("border-top","2px solid #dfe3e6").css("border-left","2px solid #dfe3e6").css("border-right","2px solid #dfe3e6").
-				css("border-radius","3px");
-	});
-
-
-
-
-
 	$(".select").on("click",function () {
 		if($(this).hasClass("unchecked")) {
 			$(".realty_search_content_select-item").css("display", "block").css("border-left","2px solid #30AE63").css("border-right","2px solid #30AE63")
@@ -151,5 +109,7 @@ $(document).ready(function(){
 	});
 
 
-	$(".select__header").selectPlugin({});
+	$(".select__header").selectPlugin({
+		autocomplete: false
+    });
 });
--- app/js/select.js
+++ app/js/select.js
@@ -19,45 +19,101 @@ if(typeof Object.create !== 'function'){
             self.options = $.extend({},$.fn.selectPlugin.options, options);
 
             self.setDesign(elem);
+            self.run();
+
+            String.prototype.startsWith = function(prefix) {
+                return this.indexOf(prefix) === 0;
+            }
         },
 
         setDesign: function () {
             var self = this;
+            var item =$(".choose__city__content_select-item");
+            var arrow = $(".selectVisibleArrow_header");
+
             self.$elem.on("click",function () {
                 if($(this).hasClass("unchecked")) {
-                    $(".choose__city__content_select-item").css("display", "block").css("border-left","2px solid #30AE63").css("border-right","2px solid #30AE63")
-                        .css("box-shadow","0 5px 10px rgba(0,0,0,0.22)");
-                    $(this).removeClass("unchecked");
-                    $(".selectVisibleArrow_header").attr("src","images/selectArrowOutwards.png");
-                    $(this).css("border-top","2px solid #30AE63").css("border-left","2px solid #30AE63").css("border-right","2px solid #30AE63").
-                    css("border-radius",0);
-                    $(".choose__city__content_select-item").last().css("border-bottom","2px solid #30AE63");
+                    item.css({
+                        "display": "block",
+                        "border-left":"2px solid #30AE63",
+                        "border-right":"2px solid #30AE63",
+                        "box-shadow":"0 5px 10px rgba(0,0,0,0.22)"
+                    }).last().css("border-bottom","2px solid #30AE63");;
+                    $(this).removeClass("unchecked").css({
+                        "border-top":"2px solid #30AE63",
+                        "border-left":"2px solid #30AE63",
+                        "border-right":"2px solid #30AE63",
+                        "border-radius":0
+                    });
+                    arrow.attr("src","images/selectArrowOutwards.png");
                 }
                 else{
-                    $(".choose__city__content_select-item").css("display", "none");
+                    item.css("display", "none");
                     $(this).addClass("unchecked");
-                    $(".selectVisibleArrow_header").attr("src","images/selectArrow.png");
+                    arrow.attr("src","images/selectArrow.png");
                     $(this).css("border-top","2px solid #dfe3e6").css("border-left","2px solid #dfe3e6").css("border-right","2px solid #dfe3e6").
                     css("border-radius","3px");
                 }
             });
-            // $(".choose__city__content_select-item").hover(function () {
-            //     $(".choose__city__content_select-item").css("border",0);
-            //     $(".select__header").css("border","2px solid #dfe3e6");
-            // });
-            // $(".choose__city__content_select-item").on("click",function () {
-            //     if($(this).hasClass("noClick")){
-            //         $(".choose__city__content_select-item").css("display", "block");
-            //     } else{
-            //         $(".choose__city__content_select-item").css("display", "none");
-            //         var selectedItem = $(this).text().trim();
-            //         $(".select__header .selectVisibleText_header").text(selectedItem);
-            //     }
-            //     $(".select__header").addClass("unchecked");
-            //     $(".selectVisibleArrow_header").attr("src","images/selectArrow.png");
-            //     $(".select__header").css("border-top","2px solid #dfe3e6").css("border-left","2px solid #dfe3e6").css("border-right","2px solid #dfe3e6").
-            //     css("border-radius","3px");
-            // });
+            item.hover(function () {
+                        item.css("border",0);
+                        self.$elem.css("border","2px solid #dfe3e6");
+                })
+                .on("click",function () {
+                    if($(this).hasClass("noClick")){
+                        item.css("display", "block");
+                    } else{
+                        var selectedItem = $(this).text().trim();
+
+                        item.css("display", "none");
+                        self.$elem.find(".selectVisibleText_header").text(selectedItem);
+                    }
+                    self.$elem.addClass("unchecked").css({
+                        "border-top":"2px solid #dfe3e6",
+                        "border-left":"2px solid #dfe3e6",
+                        "border-right":"2px solid #dfe3e6",
+                        "border-radius":"3px"
+                    });
+                    arrow.attr("src","images/selectArrow.png");
+                });
+        },
+
+        run: function(){
+            var self = this;
+
+            self.autocomplete();
+        },
+
+    autocomplete: function(){
+            var self = this;
+            var item =$(".choose__city__content_select-item");
+
+            function strStartsWith(str, prefix) {
+                return str.indexOf(prefix) === 0;
+            }
+
+            var s="teahdj";
+            var d = "te";
+            console.log(s.startsWith(d));
+
+            if(self.options.autocomplete) {
+                $(".selectItem_header input").keyup(function () {
+                    var val = $(this).val().toLowerCase();
+
+                    item.hide();
+                    item.each(function () {
+                        var text = $(this).text().toLowerCase().trim();
+                        // console.log(text,text.length,val,val.length);
+                        // console.log(text.startsWith(val));
+                        if (text.startsWith(val)|| $(this).hasClass("noClick")) {
+                            $(this).show();
+                        }
+                    });
+                });
+            }
+            else{
+                $(".noClick").find(".selectItem_header").hide();
+            }
         }
     };
 
