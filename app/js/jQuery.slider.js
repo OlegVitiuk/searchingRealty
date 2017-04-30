23b2e986db74f79a7764b4ecde85c54a04f48543
Yakovenchik <crisperok@gmail.com>
2017-04-30 13:51:57 +0300

    slider with control-buttons and non-stop autoplay

--- app/js/jQuery.slider.js
+++ app/js/jQuery.slider.js
@@ -1,94 +1,102 @@
 (function( $ ){
-  $.fn.slider = function(){
+
+$.fn.slider = function(){
       var defaults = {};
-      defaulrs.infinet = true;
-      defaulrs.index = 0;
-      defaulrs.slidesVisible = 1;
-      defaulrs.slidesToScroll = 1;
-      defaulrs.arrows = true;
-      defaulrs.swipeAble = true;
-      defaulrs.pagination = true;
-      defaulrs.autoplay = true;
-      defaulrs.autoplaySpeed = 5000;
+      defaults.infinet = true;
+      defaults.index = 0;
+      defaults.slidesVisible = 1;
+      defaults.slidesToScroll = 1;
+      defaults.arrows = true;
+      defaults.swipeAble = true;
+      defaults.pagination = true;
+      defaults.autoplay = true;
+      defaults.autoplaySpeed = 1000;
   
-    var options = $.extend({}, defaults, options, params);
+    var options = $.extend({}, defaults, options);
     var elem = this;
     var elemWidth = 202;
     var slides = $('.slide');
     var numberofElem = length.slides;
-    $('#slidesContainer').css('overflow', 'hidden');
-    
-    $('#slidesContainer>div:last-child').prependTo('#slidesContainer');
+    //$('#slidesContainer').css('overflow', 'hidden');
+      $('#slidesContainer').css('left','0px');
+      $('#slidesContainer').css('position','relative');
+      $('#slidesContainer').css('width','elemWidth*numberofElem');
+   // $('#slidesContainer>div:last-child').prependTo('#slidesContainer');
 
     slidesVisible = elem.width/elemWidth>>0;
-
-      $('<span class = "control" id = "control_prev">Move Left</span>').prependTo('#slidesContainer');
-      $('<span class ="control" id = "control_next">Move Right</span>').appendTo('#slidesContainer');
-
-
-    $('.control').bind('click', function(){
-          if(elem.attr('id')=='control_next'){
+      $('<span class ="control_next">Next</span>').appendTo('#slidesShow');
+      $('<span class = "control_prev">Prev</span>').prependTo('#slidesShow');
+     
+ $('.control_next').click(function(){
               $('#slidesContainer').animate({
                 left: -elemWidth
-               }, 200, function(){
-                 option.index != 0
-               ? options.index-1 : option.index = numberofElem-1;
-                $('#slidesContainer div:last-child').prependTo('#slidesContainer');
-               })
-             } else{
+               }, 1000, function(){
+                 $('#slidesContainer>div:first-child').appendTo('#slidesContainer');
+                 options.index != 0
+               ? options.index-1 : options.index = numberofElem-1;
+                $('#slidesContainer').css('left','0px')
+                });
+             }); 
+    $('.control_prev').click(function(){
                 $('#slidesContainer').animate({
-                left: +elemWidth
-               }, 200, function(){
-                 option.index != numberofElem-1
-               ? options.index+1 : option.index = 0;
-                $('#slidesContainer div:first-child').appendTo('#slidesContainer');
+                left: +elemWidth    
+               }, 1000, function(){
+              $('#slidesContainer>div:last-child').prependTo('#slidesContainer');
+                 options.index != numberofElem-1
+               ? options.index+1 : options.index = 0;
+                  $('#slidesContainer').css('left','0px')
+
                });
-             };
-});
+             });
+
 $( window ).on( "swipe", function( event ) { 
   var swipestart = touchstart(x);
   var swipeend = touchend(x);
   $('slidesContainer').animate({
     left: +swipestart-swipeend
-  }, 200, function(){
+  }, 1000, function(){
     for(var i = 1; i<=math.abs((swipestart-swipeend)/elemWidth >>0); i++){
       if ((swipestart-swipeend)<0){
         $('#slidesContainer').animate({
                 left: -elemWidth
-               }, 200, function(){
-                 option.index != 0
-               ? options.index-1 : option.index = numberofElem-1;
-                $('#slidesContainer div:last-child').prependTo('#slidesContainer');
-               })
+               }, 1000, function(){
+                 options.index != 0
+               ? options.index-1 : options.index = numberofElem-1;
+                $('#slidesContainer>div:first-child').appendTo('#slidesContainer');
+                $('#slidesContainer').css('left','0px')
+                   })
       }else{
         $('#slidesContainer').animate({
                 left: +elemWidth
-               }, 200, function(){
-                 option.index != numberofElem-1
-               ? options.index+1 : option.index = 0;
-                $('#slidesContainer div:first-child').appendTo('#slidesContainer');
+               }, 1000, function(){
+                 options.index != numberofElem-1
+               ? options.index+1 : options.index = 0;
+                $('#slidesContainer>div:last-child').prependTo('#slidesContainer');
+                $('#slidesContainer').css('left','0px')
                });
       };
     };
   });
 } );
         
-   // for(var i = 1; i<=(elem.width/elemWidth >>0); i++){
-
-    //}
 
 
-$('.slidesContainer').mouseleave(function(){
+$('#slidesContainer').mouseout(function(){
   setInterval(function () {
     $('#slidesContainer').animate({
                 left: -elemWidth
-               }, 200, function(){
-                 option.index != 1
-               ? options.index-1 : option.index = numberofElem;
-                $('#slidesContainer div:last-child').prependTo('#slidesContainer');
+               }, 1000, function(){
+                 options.index != 1
+               ? options.index-1 : optionsindex = numberofElem;
+                 $('#slidesContainer>div:first-child').appendTo('#slidesContainer');
+                 $('#slidesContainer').css('left','0px')
                })
   }, options.autoplaySpeed);
 });
 
-  }
-})(jQuery)
+};
+}(jQuery))
+
+$(document).ready(function(){
+  $('.slidesShow').slider();
+})
